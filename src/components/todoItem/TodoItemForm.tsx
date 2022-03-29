34f27cd78cdfecc69920354cf7203b99d328bfc3
Max Kelly <maxwill.kelly@gmail.com>
2022-03-29 23:33:30 +0100

    fix(reject-empty-todos): Rejects empty todos and displays errors
    Displays a red error outline and message on text inputs when the form is submitted with an empty title or description

--- src/components/todoItem/TodoItemForm.tsx
+++ src/components/todoItem/TodoItemForm.tsx
@@ -1,40 +1,50 @@
 import React from "react";
+import { Control, Controller } from "react-hook-form";
 
 import TextField from "@mui/material/TextField";
 
 import { TodoItem } from "./TodoItem";
 
 interface Props {
-  todoItem: TodoItem;
-  onChange: (updatedTodoItem: TodoItem) => void;
+  control: Control<TodoItem, unknown>;
 }
 
 // The todo form that appears inside the create modal, could be used for edit too perhaps?
-const TodoItemForm = ({ todoItem, onChange }: Props) => {
-  const { title, description } = todoItem;
-
+const TodoItemForm = ({ control }: Props) => {
   return (
     <>
-      <TextField
-        sx={{ mt: 1, mb: 2 }}
-        fullWidth
-        variant="outlined"
-        label="Title"
-        value={title}
-        onChange={(event) => {
-          onChange({ ...todoItem, title: event.target.value });
-        }}
+      <Controller
+        name="title"
+        control={control}
+        rules={{ required: true }}
+        render={({ field, fieldState: { error } }) => (
+          <TextField
+            sx={{ mt: 1, mb: 2 }}
+            fullWidth
+            variant="outlined"
+            label="Title"
+            {...field}
+            error={Boolean(error)}
+            helperText={error && "A title is required"}
+          />
+        )}
       />
-      <TextField
-        fullWidth
-        variant="outlined"
-        label="Description"
-        multiline
-        maxRows={4}
-        value={description}
-        onChange={(event) => {
-          onChange({ ...todoItem, description: event.target.value });
-        }}
+      <Controller
+        name="description"
+        control={control}
+        rules={{ required: true }}
+        render={({ field, fieldState: { error } }) => (
+          <TextField
+            fullWidth
+            variant="outlined"
+            label="Description"
+            multiline
+            maxRows={4}
+            {...field}
+            error={Boolean(error)}
+            helperText={error && "A description is required"}
+          />
+        )}
       />
     </>
   );
